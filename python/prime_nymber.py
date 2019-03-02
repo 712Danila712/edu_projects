f928310177adf6ad6fbf17ab78229fe7dc5ae510
Danya <motin.vasily@gmail.com>
2019-03-02 14:19:43 +0300

    prime_number work

--- python/prime_nymber.py
+++ python/prime_nymber.py
@@ -1,10 +1,17 @@
+import math
+
+
+def isPrime(n):
+    i = 2
+    while i <= math.sqrt(n):
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+
 n = int(input())
-i = 0
 
-while i < n:
-    if n % i == 0:
-        print("NO")
-        exit(0)
-    i += 1
-
-print("YES")
+for i in range (2, n):
+    if isPrime(i):
+        print(i, end=' ')
