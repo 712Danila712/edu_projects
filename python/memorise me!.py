50b48a0493840cec301b785949ed2e8a0499e3f4
Danya <motin.vasily@gmail.com>
2019-03-08 11:36:42 +0300

    memorise me end!

--- python/memorise me!.py
+++ python/memorise me!.py
@@ -1,9 +1,15 @@
 N = int(input())
 a = list(map(int, input().split()))
+lst = [0 for i in range(1001)]
+
 Q = int(input())
+
+for i in a:
+    lst[i] += 1
+
 for i in range(Q):
-    q = int(input)
-    if q not in a:
+    q = lst[int(input())]
+    if q == 0:
         print("NOT PRESENT")
     else:
-        print(a.count(q))
+        print(q)
