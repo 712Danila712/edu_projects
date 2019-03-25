a525f727104b67cad1d6a2ed6474f3adcfb5c679
Danya <motin.vasily@gmail.com>
2019-03-25 22:35:29 +0300

    Added Stepik_3.2_1

---
+++ python/Stepik_3.2_1.py
@@ -0,0 +1,10 @@
+def update_dictionary(d, key, value):
+    if key in d:
+        d[key] += [value]
+    else:
+        if 2 * key in d:
+            d[2 * key] += [value]
+        else:
+             d.update({2 * key: [value]})
+        
+
