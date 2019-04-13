972df725655481bf44cd615ea147df4b07bdf1db
Danya <motin.vasily@gmail.com>
2019-04-13 12:16:19 +0300

    work with Help_Jarvis

--- python/Help_Jarvis!.py
+++ python/Help_Jarvis!.py
@@ -1,26 +1,16 @@
 n = int(input())
-lst = []
-l = [int(input()) for j in range(n)]
-for i in l:
-    while i > 0:
-        lst.append(i % 10)
-        i //= 10
-    lst.sort()
-    for k in range(len(lst)):
-        if k == len(lst) - 1:
-            if lst[-1] - lst[-2] > 1:
-                print("NO")
-                break
-            else:
-                print("YES")
-        else:
-            if lst[k + 1] - lst[k] > 1:
-                print("NO")
-                break
-            else:
-                print("YES")
-        
-            
-        
+
+for i in range(n):
+    l = input()
     lst = []
 
+    for k in l:
+        lst.append(int(k))
+    lst.sort()
+
+    for j in range(len(lst) - 1):
+        if lst[j + 1] - lst[j] != 1:
+            print("NO")
+            break
+    else:
+        print("YES")
