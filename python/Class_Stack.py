644b9e0484336e9ca05d01d87573838a72bca5b9
Danya <motin.vasily@gmail.com>
2019-04-13 16:54:13 +0300

    work with Stack

--- python/Class_Stack.py
+++ python/Class_Stack.py
@@ -5,8 +5,42 @@ class Stack:
         self.top = 0
         self.count = 0
 
-    #def push(self, item):
-     #   if self.top + 1 == self.size:
-        
-a = Stack()
-print("{} {} {} {}".format(a.size, a.content, a.top, a.count))
+    def push(self, item):
+        if self.top + 1 == self.size + 1:
+            print("Lack of space")
+        else:
+            self.content.append(item)
+            self.top += 1
+            self.count += 1
+
+    def pop(self, smh):
+        if self.top - 1 == 0:
+            print("Out of space")
+        else:
+            if smh in self.content:
+                self.content.remove(smh)
+                self.top -= 1
+                self.count -= 1
+
+    def write(self):
+        print("size:{} content:{} top:{}".format(self.size, self.content, self.top))
+
+    def clear(self):
+        del self.content[0:]
+
+
+if __name__ == '__main__':
+    a = Stack()
+
+    a.size = 6
+
+    for i in range(9):
+        a.push(7)
+
+    a.write()
+
+    for i in range(9):
+        a.pop(7)
+    a.clear()
+
+    a.write()
